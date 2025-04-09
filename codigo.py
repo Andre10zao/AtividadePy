2b1dc35db758d58aa13d629fdc72fb8b442ffaad
Andre10zao <144570018+Andre10zao@users.noreply.github.com>
2025-04-08 21:10:47 -0300

    Add files via upload

---
+++ codigo.py
@@ -0,0 +1,6 @@
+# Lista com os valores de vendas realizadas em cada semana do mês
+vendas_mensais = [1000, 2000, 3000]
+
+# Função que recebe uma lista de valores e retorna a soma total
+def calcular_total_vendas(lista):
+    return sum(lista)
